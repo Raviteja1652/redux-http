4f409da3145e8e05c6282df2b7ca3d8ce7509410
Raviteja1652 <ravitejamrt.165@gmail.com>
2024-07-31 04:50:54 +0530

    sending network calls and saving data in firebase

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-
+  const cart = useSelector(state => state.cart)
   const toggle = useSelector(state => state.toggle.toggle)
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await axios.put('https://api-calls-prep-default-rtdb.firebaseio.com/cart-http.json', cart);
+        const data = await res.data
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      };
+    };
+    fetchData()
+  }, [cart])
 
   return (
     <Layout>
