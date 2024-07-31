5b6e5e3122608bc32b511b62e218211adcfc3529
Raviteja1652 <ravitejamrt.165@gmail.com>
2024-07-31 06:37:43 +0530

    Handling HTTP states and Feedback with Redux using useEffect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,63 @@
-import { useSelector } from 'react-redux';
+import { Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useEffect } from 'react';
 import axios from 'axios';
+import Notification from './components/UI/Notification';
+import { toggleActions } from './Store/toggle-slice';
+
+let init_notif = true;
 
 function App() {
   const cart = useSelector(state => state.cart)
   const toggle = useSelector(state => state.toggle.toggle)
+  const notification = useSelector(state => state.toggle.notification)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     async function fetchData() {
-      try {
-        const res = await axios.put('https://api-calls-prep-default-rtdb.firebaseio.com/cart-http.json', cart);
-        const data = await res.data
-        console.log(data)
-      } catch (error) {
-        console.log(error)
-      };
+      dispatch(toggleActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart Data!'
+      }));
+      
+      const res = await axios.put('https://api-calls-prep-default-rtdb.firebaseio.com/cart-http.json', cart);
+      const data = await res.data
+      console.log(data)
+
+      dispatch(toggleActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent Data Successfully'
+      }))
     };
-    fetchData()
-  }, [cart])
+
+    if (init_notif) {
+      init_notif = false
+      return
+    }
+
+    fetchData().catch(err => {
+      dispatch(toggleActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: 'Sending Data Failed'
+      }))
+    })
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      {toggle && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message}/>}
+      <Layout>
+        {toggle && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/Store/toggle-slice.js
+++ src/Store/toggle-slice.js
@@ -1,12 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { toggle: false }
+const initialState = { toggle: false, notification: null }
 const toggleSlice = createSlice({
     name: 'toggle',
     initialState: initialState,
     reducers: {
         toggleCart (state) {
             state.toggle = !state.toggle
+        },
+        showNotification(state, action) {
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message
+            }
         }
     }
 })
